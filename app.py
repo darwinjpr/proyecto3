0c3c309863ebead61970e2011ab628d547bc6935
Darwin <darwinjpr@hotmail.com>
2020-08-13 23:15:35 -0600

    update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 import time
 import tkinter as tk
-#import subprocess #para ejecutar en terminal de computador
+import subprocess #para ejecutar en terminal de computador
 import paramiko #para ejecutar remoto
 from getpass import getpass
 
@@ -34,22 +34,6 @@ class Application(tk.Frame):
 		self.connect_ssh["text"] = "Comprobar datos de conexion."
 		self.connect_ssh["command"] = self.connect
 
-		self.start_app = tk.Button(self)
-		self.start_app["text"] = "Iniciar aplicacion."
-		self.start_app["command"] = self.startapp
-
-		self.read_inf = tk.Button(self)
-		self.read_inf["text"] = "Leer informe."
-		self.read_inf["command"] = self.read
-
-		self.etiq_video = tk.Label(self, text="Video a ver:", font="Arial")
-		self.video = str   
-		self.ctext_video = tk.Entry(self, textvariable=self.video,width=30)
-
-		self.watch_video = tk.Button(self)
-		self.watch_video["text"] = "Ver video."
-		self.watch_video["command"] = self.watchvideo
-
 		self.quit = tk.Button(self, text="SALIR", fg="red")
 		self.quit["command"] = self.exit
 
@@ -66,68 +50,75 @@ class Application(tk.Frame):
 
 		self.connect_ssh.grid(column=0, row=4, columnspan=3)
 
-		self.start_app.grid(column=0, row=5, columnspan=3)
-
-		self.read_inf.grid(column=0, row=6, columnspan=3)
-
-		self.etiq_video.grid(column=0, row=7)
-		self.ctext_video.grid(column=1, row=7, columnspan=2)
-
-		self.watch_video.grid(column=0, row=8, columnspan=3)
-
-		self.quit.grid(column=0, row=9, columnspan=3)
+		self.quit.grid(column=0, row=5, columnspan=3)
 
 	def say_hi(self):
 		print("\nHola!\n\nIntegrantes:\nDarwin\nEdwin\nAlonso\nKarol")
 
 	def connect(self):#Establece conexion via ssh
-		print("\nSe establece la conexion!")
-		client=paramiko.SSHClient()
-		client.set_missing_host_key_policy( paramiko.AutoAddPolicy() )
-		client.connect(self.ctext_ip.get(), username=self.ctext_user.get(), password=self.ctext_clave.get())
+		self.client=paramiko.SSHClient()
+		self.client.set_missing_host_key_policy( paramiko.AutoAddPolicy() )
+		self.client.connect('192.168.0.107', username='darwin', password='holahola')
+		self.sftp = self.client.open_sftp()
+		#self.client.connect(self.ctext_ip.get(), username=self.ctext_user.get(), password=self.ctext_clave.get())
 		print("\nConectado!")
-		stdin, stdout, stderr = client.exec_command('ls')
-		result = stdout.read().decode()
-		print(result)
-		print("\nLa conexion es correcta.")
-		print("\nCerrando conexion.")
-		client.close()
+		self.create_widgets_connect()
+		
 
-	def startapp(self):#para iniciar sniffer (sin hacer)
+	def create_widgets_connect(self):
+		self.connect_interface = tk.Toplevel(self)
+		self.connect_interface.geometry("400x400")
+		self.connect_interface.resizable(0,0)
+
+		self.start_app = tk.Button(self.connect_interface)
+		self.start_app["text"] = "Iniciar aplicacion."
+		self.start_app["command"] = self.startapp
+		self.start_app.grid(column=1, row=0, columnspan=3)
+
+		self.read_inf = tk.Button(self.connect_interface)
+		self.read_inf["text"] = "Leer informe."
+		self.read_inf["command"] = self.read
+		self.read_inf.grid(column=1, row=1, columnspan=3)
+
+		self.etiq_video = tk.Label(self.connect_interface, text="Video a ver:", font="Arial")
+		self.etiq_video.grid(column=1, row=2)
+
+		self.video = str   
+		self.ctext_video = tk.Entry(self.connect_interface, textvariable=self.video,width=30)
+		self.ctext_video.grid(column=2, row=2, columnspan=2)
+
+		self.watch_video = tk.Button(self.connect_interface)
+		self.watch_video["text"] = "Ver video."
+		self.watch_video["command"] = self.watchvideo
+		self.watch_video.grid(column=1, row=3, columnspan=3)
+
+		self.quit2 = tk.Button(self.connect_interface, text="SALIR", fg="red")
+		self.quit2["command"] = self.disconnect
+		self.quit2.grid(column=1, row=4, columnspan=3)
+
+		self.master.wait_window(self.connect_interface)
+
+	def startapp(self):#para iniciar sniffer
 		print("\nIniciando aplicacion!")
-		client=paramiko.SSHClient()
-		client.set_missing_host_key_policy( paramiko.AutoAddPolicy() )
-		client.connect(self.ctext_ip.get(), username=self.ctext_user.get(), password=self.ctext_clave.get())
-		print("\nConectado!")
-		stdin, stdout, stderr = client.exec_command('python3 /usr/bin/python-sniffer.py')
+		print("\nPresione ESC para salir!")
+		stdin, stdout, stderr = self.client.exec_command('python3 Descargas/apple.py')
 		result = stdout.read().decode()
 		print(result)
-		print("\nCerrando conexion.")
-		client.close()
 
 	def read(self):#Para leer informe generado por el sniffer (sin hacer)
 		print("\nAbriendo informe!")
-		client=paramiko.SSHClient()
-		client.set_missing_host_key_policy( paramiko.AutoAddPolicy() )
-		client.connect(self.ctext_ip.get(), username=self.ctext_user.get(), password=self.ctext_clave.get())
-		print("\nConectado!")
-		stdin, stdout, stderr = client.exec_command('nano -v /usr/bin/Informe.txt') #abre solo lectura
-		result = stdout.read().decode()
-		print(result)
-		print("\nCerrando conexion.")
-		client.close()
+		self.sftp.get('/home/darwin/Descargas/Informe.txt', '~/proyecto3/inform.txt')
+		subprocess.call(['nano','-v','inform.txt'])
 
 	def watchvideo(self):#Para ver un video generado por el sniffer (sin hacer)
 		print("\nReproduciendo video!")
-		client=paramiko.SSHClient()
-		client.set_missing_host_key_policy( paramiko.AutoAddPolicy() )
-		client.connect(self.ctext_ip.get(), username=self.ctext_user.get(), password=self.ctext_clave.get())
-		print("\nConectado!")
-		stdin, stdout, stderr = client.exec_command(['omxplayer', "/usr/bin/"+self.ctext_video.get()])
-		result = stdout.read().decode()
-		print(result)
-		print("\nCerrando conexion.")
-		client.close()
+		self.sftp.get('/home/darwin/Descargas/Corto_Animado29.avi', '/home/darwin/proyecto3/Corto1.avi')
+		subprocess.call(['mplayer','Corto1.avi'])
+
+	def disconnect(self):#Salir de la app
+		print("\nCerrando conexion!")
+		self.connect_interface.destroy()
+		self.client.close()
 
 	def exit(self):#Salir de la app
 		print("\nSaliendo!\n")
